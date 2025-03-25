a63f5a55a8d3fcd24a2c39fad8c0429e230e0903
Kevin Jiang <kevinjiang121@gmail.com>
2025-03-24 21:32:05 -0400

    added get endpoint for channels

--- moonbase_backend/apps/channels/urls.py
+++ moonbase_backend/apps/channels/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CreateChannelView, DeleteChannelView, CreateChannelGroupView, DeleteChannelGroupView
+from .views import (
+    CreateChannelView, DeleteChannelView,
+    CreateChannelGroupView, DeleteChannelGroupView,
+    GetChannelView, GetChannelGroupView,
+    GetChannelsListView, GetChannelGroupsListView
+)
 
 urlpatterns = [
     path('create-channel/', CreateChannelView.as_view(), name='create-channel'),
     path('delete-channel/<int:pk>/', DeleteChannelView.as_view(), name='delete-channel'),
     path('create-channel-group/', CreateChannelGroupView.as_view(), name='create-channel-group'),
     path('delete-channel-group/<int:pk>/', DeleteChannelGroupView.as_view(), name='delete-channel-group'),
+    path('get-channel/<int:pk>/', GetChannelView.as_view(), name='get-channel'),
+    path('get-channel-group/<int:pk>/', GetChannelGroupView.as_view(), name='get-channel-group'),
+    path('get-channels-list/', GetChannelsListView.as_view(), name='get-channels-list'),
+    path('get-channel-groups-list/', GetChannelGroupsListView.as_view(), name='get-channel-groups-list'),
+
 ]
--- moonbase_backend/apps/channels/views.py
+++ moonbase_backend/apps/channels/views.py
@@ -5,8 +5,8 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Channel, ChannelGroup
 from .serializers import ChannelSerializer, ChannelGroupSerializer
+from rest_framework import generics
 
-# Endpoint to create a channel
 class CreateChannelView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = ChannelSerializer(data=request.data)
@@ -15,14 +15,12 @@ class CreateChannelView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Endpoint to delete a channel by its primary key
 class DeleteChannelView(APIView):
     def delete(self, request, pk, *args, **kwargs):
         channel = get_object_or_404(Channel, pk=pk)
         channel.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-# Endpoint to create a channel group
 class CreateChannelGroupView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = ChannelGroupSerializer(data=request.data)
@@ -31,9 +29,28 @@ class CreateChannelGroupView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Endpoint to delete a channel group by its primary key
 class DeleteChannelGroupView(APIView):
     def delete(self, request, pk, *args, **kwargs):
         channel_group = get_object_or_404(ChannelGroup, pk=pk)
         channel_group.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class GetChannelView(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        channel = get_object_or_404(Channel, pk=pk)
+        serializer = ChannelSerializer(channel)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class GetChannelGroupView(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        channel_group = get_object_or_404(ChannelGroup, pk=pk)
+        serializer = ChannelGroupSerializer(channel_group)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class GetChannelsListView(generics.ListAPIView):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+
+class GetChannelGroupsListView(generics.ListAPIView):
+    queryset = ChannelGroup.objects.all()
+    serializer_class = ChannelGroupSerializer
