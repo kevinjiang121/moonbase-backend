daa520c38bf7ea7d8153701659257d2f303d99df
Kevin Jiang <kevinjiang121@gmail.com>
2025-04-02 18:17:18 -0400

    redid consumers to use user id and channel id

--- moonbase_backend/apps/chats/consumers.py
+++ moonbase_backend/apps/chats/consumers.py
@@ -9,8 +9,8 @@ User = get_user_model()
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
+        self.channel_id = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.channel_id}'
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -24,39 +24,37 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data):
+        if not text_data.strip():
+            return
+
         try:
             data = json.loads(text_data)
         except json.JSONDecodeError:
             return
         
-        message = data.get('message', '').strip()
-        if not message:
-            return
-
-        username = data.get('username')
-        await self.save_chat(message, username)
+        message = data.get('message')
+        user_id = data.get('user_id')
+        await self.save_chat(message, user_id)
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'username': username,
+                'user_id': user_id,
             }
         )
 
-
     async def chat_message(self, event):
         message = event.get('message')
-        username = event.get('username')
+        user_id = event.get('user_id')
         await self.send(text_data=json.dumps({
             'message': message,
-            'username': username,
+            'user_id': user_id,
         }))
 
     @database_sync_to_async
-    def save_chat(self, message, username):
-        channel_obj = Channel.objects.filter(name=self.room_name).first()
-        user_obj = User.objects.filter(username=username).first()
-        print(user_obj)
+    def save_chat(self, message, user_id):
+        channel_obj = Channel.objects.filter(id=self.channel_id).first()
+        user_obj = User.objects.filter(user_id=user_id).first()
         if channel_obj and user_obj:
             Chat.objects.create(channel=channel_obj, author=user_obj, content=message)
